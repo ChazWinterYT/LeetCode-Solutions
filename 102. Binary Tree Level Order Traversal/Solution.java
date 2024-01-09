85f31512024214f8a383428540f8fca4cb174b81
Chaz Winter <139367543+ChazWinterYT@users.noreply.github.com>
2024-01-09 18:34:04 -0500

    Java Solution
            Submission Statistics:
            Question #: 102
            Language: Java
            Runtime: 1 ms
            Runtime percentile: 89.76
            Memory: 44.84 MB
            Memory percentile: 23.37

---
+++ 102. Binary Tree Level Order Traversal/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+                if (current.left != null) queue.add(current.left);
+                if (current.right != null) queue.add(current.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
