bc1d704b80b217663948780c8f2db789a433698a
Chaz Winter <139367543+ChazWinterYT@users.noreply.github.com>
2024-01-09 18:37:55 -0500

    Java Solution
            Submission Statistics:
            Question #: 111
            Language: Java
            Runtime: 6 ms
            Runtime percentile: 25.43
            Memory: 63.788 MB
            Memory percentile: 6.41

---
+++ 111. Minimum Depth of Binary Tree/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        
+        if (root.left == null) {
+            return 1 + rightDepth;
+        }
+        if (root.right == null) {
+            return 1 + leftDepth;
+        }
+        
+        return 1 + Math.min(leftDepth, rightDepth);
+    }
+}
