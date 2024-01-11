8d526bc91cd4e05677296be6d62ca8cb5d4499d0
Chaz Winter <139367543+ChazWinterYT@users.noreply.github.com>
2024-01-11 01:02:04 -0500

    Java Solution
            Submission Statistics:
            Question #: 139
            Language: Java
            Runtime: 8 ms
            Runtime percentile: 40.20
            Memory: 44.32 MB
            Memory percentile: 20.60

---
+++ 139. Word Break/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true; // Base case
+        
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
