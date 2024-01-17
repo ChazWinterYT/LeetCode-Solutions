3b62ab2750484848cb131bb940f287847985c6f9
Chaz Winter <139367543+ChazWinterYT@users.noreply.github.com>
2024-01-17 12:39:42 -0500

    Java Solution
            Submission Statistics:
            Question #: 232
            Language: Java
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 41.948 MB
            Memory percentile: 7.53

---
+++ 232. Implement Queue using Stacks/Solution.java
@@ -0,0 +1,40 @@
+class MyQueue {
+    Stack<Integer> inputStack;
+    Stack<Integer> outputStack;
+    
+    public MyQueue() {
+        inputStack = new Stack<>();
+        outputStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        inputStack.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return outputStack.pop();
+    }
+    
+    public int peek() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+        return outputStack.peek();
+    }
+    
+    public boolean empty() {
+        return outputStack.isEmpty() && inputStack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
