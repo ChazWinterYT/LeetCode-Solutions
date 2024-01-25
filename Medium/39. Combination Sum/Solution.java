784e9c47e991e6ac16222c10870e0561ce1bf6f3
ChazWinterYT <chaz@hmrmusic.com>
2024-01-25 13:59:48 -0500

    Sorted solutions according to difficulty

--- Medium/39. Combination Sum/Solution.java
+++ Medium/39. Combination Sum/Solution.java
@@ -1,22 +1,24 @@
 class Solution {
+    List<List<Integer>> results = new ArrayList<>();
+    int[] candidates;
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        dfs(candidates, target, 0, new ArrayList<>(), results);
+        this.candidates = candidates;
+        dfs(0, target, new ArrayList<>());
         return results;
     }
     
-    private void dfs(int[] candidates, int target, int index, 
-                    List<Integer> combination, List<List<Integer>> results) {
+    private void dfs(int index, int target, List<Integer> current) {
         if (target == 0) {
-            results.add(new ArrayList<>(combination));
+            results.add(new ArrayList<>(current));
             return;
         }
         for (int i = index; i < candidates.length; i++) {
             if (candidates[i] <= target) {
-                combination.add(candidates[i]);
-                dfs(candidates, target - candidates[i], i, combination, results);
+                current.add(candidates[i]);
+                dfs(i, target - candidates[i], current);
                 // If it fails, remove the last value from the potential list before trying the next one
-                combination.remove(combination.size() - 1);
+                current.remove(current.size() - 1);
             }
         }
     }
