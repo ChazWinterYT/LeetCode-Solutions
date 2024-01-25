03a7b111d56744fcbcc01fdd5e77660761adc2c3
ChazWinterYT <chaz@hmrmusic.com>
2024-01-25 12:27:56 -0500

    Sorted solutions according to difficulty

--- Medium/46. Permutations/Solution.java
+++ Medium/46. Permutations/Solution.java
@@ -1,19 +1,26 @@
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    int[] nums;
+    
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, nums, new ArrayList<>());
+        this.nums = nums;
+        backtrack(new ArrayList<>());
         return result;
     }
     
-    private void backtrack(List<List<Integer>> result, int[] nums, List<Integer> tempList) {
-        if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                if (tempList.contains(nums[i])) continue;
-                tempList.add(nums[i]);
-                backtrack(result, nums, tempList);
-                tempList.remove(tempList.size() - 1);
+    private void backtrack(List<Integer> current) {
+        // Base case: if your list has all the numbers, submit it
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            // Inclusion case: only run this block if the current number is not already in the sublist
+            if (!current.contains(nums[i])) {
+                current.add(nums[i]);
+                backtrack(current);
+                current.remove(current.size() - 1);
             }
         }
     }
