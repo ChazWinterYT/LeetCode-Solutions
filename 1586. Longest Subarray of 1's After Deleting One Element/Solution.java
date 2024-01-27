29f0d15468d0d8b735e5f1b6daea13e5287ca9c8
Chaz Winter <139367543+ChazWinterYT@users.noreply.github.com>
2024-01-26 21:16:33 -0500

    Java Solution
            Submission Statistics:
            Question #: 1586
            Language: Java
            Runtime: 3 ms
            Runtime percentile: 70.97
            Memory: 56.864 MB
            Memory percentile: 36.24

--- 1586. Longest Subarray of 1's After Deleting One Element/Solution.java
+++ 1586. Longest Subarray of 1's After Deleting One Element/Solution.java
@@ -3,28 +3,26 @@ class Solution {
         int numZeros = 0;
         int max = 0;
         
-        if (nums[0] == 0) {
-            numZeros = 1;
+        // Two pointers. Expand with the right pointer if 0-1 zeros present.
+        // Shrink with left pointer if >1 zeros present. Distance between pointers
+        // is the subarray, which we should check for max length.
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                numZeros++;
+            }
+            // Doing the above may have caused the zero count to go over the limit. Fix this.
+            while (numZeros > 1) {
+                if (nums[left++] == 0) {
+                    numZeros--;
+                }
+            }
+            // You should have a valid subarray at this point. Check its length.
+            // Since we are required to delete one element, (right - left) is accurate.
+            max = Math.max(max, right - left);
         }
         
-        // Two pointers. Expand with the right pointer if 0-1 zeros present.
-        // Shrink with left pointer with >1 zeros present. Distance between pointers
-        // is the subarray, which we should check for max length.
-        int left = 0, right = 0;
-        while (right < nums.length) {
-            // Expand with the right pointer if we don't have too many zeros. Also count the new value
-            // and check the subarray length. Since we must delete one value, right - left is accurate.
-            if (numZeros <= 1) {
-                max = Math.max(max, right - left);
-                right++;
-                if (right < nums.length && nums[right] == 0) numZeros++;
-            } 
-            // Doing the above may have caused the zero count to go over the limit. Check this.
-            if (numZeros > 1) {
-                // Decrement zero count if the value leaving was a zero.
-                if (nums[left++] == 0) numZeros--;
-            }
-        }
+        
         return max;
     }
 }
